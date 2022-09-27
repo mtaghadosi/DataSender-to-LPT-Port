0204a8d0b94113ba88bde52c9d22d0531088a1b3
Mohammad Taghadosi <mohammad.taghadosi@gmail.com>
2022-09-27 16:43:10 +0330

    Add files via upload

---
+++ ShowLEDsfrm.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LPT
+{
+    public partial class ShowLEDsfrm : Form
+    {
+        public ShowLEDsfrm()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowLEDsfrm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
